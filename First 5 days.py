28341911667800f36ff28abcf3796d625270e030
Razan Alaskar <razanfahad@Razans-MacBook-Pro.local>
2019-08-23 13:06:32 +0300

    Create First 5 days.py

---
+++ First 5 days.py
@@ -0,0 +1,54 @@
+# Day 1:
+print("Hello World")
+
+
+
+"""
+Day 2:
+Lesson Topic: 
+Comments 
+"""
+
+
+# Day 3:
+x=10 # type: int
+y="Razan" #type: string
+print(x)
+print(y)
+
+x=y=10
+print(x+y)
+
+num , name = '10' , "Razan"
+print("My name is"+name)
+print("My favorite number is"+num)
+
+hello="Hello"
+name=" Everyone"
+welcome=hello+name
+print(welcome)
+
+
+#Day 4:
+x=1
+y=2.5
+z=3+6j
+print(type(x))
+print(type(y))
+print(type(z))
+
+x=complex(x)
+print(type(x))
+
+import random
+
+print(random.randrange(1,10))
+print(random.randrange(1,10))
+print(random.randrange(1,10))
+
+#Day 5:
+x="apple "
+y="orange "
+z="limon "
+basket = x + y + z
+print(basket)
